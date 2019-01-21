caa28bf75501b48c6739f53c11754a9cb1d05a3f
followkujianingithub <948244321@qq.com>
2019-01-21 17:44:05 +0800

    tian jia ws

--- client/src/components/login.vue
+++ client/src/components/login.vue
@@ -15,7 +15,6 @@
 import { hex_md5 } from "../util/md5";
 import httpHelper from "../util/httpHelper";
 import { setTelPhone } from "../util/cacheManger";
-
 export default {
   data() {
     return {
@@ -54,6 +53,34 @@ export default {
       let _self = this;
       if (_self.phone && _self.code) {
         setTelPhone(_self.phone);
+        //建立连接
+        let webSocket = new WebSocket("ws://localhost:8001/");
+        let opendata, closedata, messagedata;
+        //开启连接
+        webSocket.onopen = function(e) {
+          opendata = e;
+          console.log("webSocket open", e);
+          document.getElementById("recv").innerHtml(JSON.parse(e.data));
+        };
+        //关闭连接
+        // webSocket.onclose = function(e) {
+        //   closedata = e;
+        //   console.log("webSocket close", e);
+        //   document.getElementById("recv").innerHtml(JSON.parse(e.data));
+        // };
+
+        //拿到返回
+        webSocket.onmessage = function(e) {
+          messagedata = e;
+          console.log("webSocket close", e);
+          document.getElementById("recv").innerHtml(JSON.parse(e.data));
+        };
+
+        //发送信息
+        // document.getElementById("sendBtn").onclick = function() {
+        //   var text = document.getElementById("sendTxt").value;
+        //   webSocket.send(text);
+        // };
         window.location.href = "/users.html";
       }
     }

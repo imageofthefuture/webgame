619a1b3cd648c8dc490742e49563b2c5e334349e
followkujianingithub <948244321@qq.com>
2019-01-24 08:44:22 +0800

    fuwuqi ws

--- server/lib/websocket.js
+++ server/lib/websocket.js
@@ -1,5 +1,6 @@
-const io = require('socket.io')(http);
+
 exports.start = function (http) {
+    const io = require('socket.io')(http);
     io.on('connection', function (socket) {
         socket.on('message', function (obj) {
             obj.username = "fuwuqi:" + obj.username;
